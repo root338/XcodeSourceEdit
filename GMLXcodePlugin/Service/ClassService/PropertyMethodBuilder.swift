9c138fe98ec3c20980c61538dafc5407ffb4b9d3
GML <a.chen.guang>
2021-05-15 18:31:33 +0800

    修改选中的行忽略了最后一行

--- GMLXcodePlugin/Service/ClassService/PropertyMethodBuilder.swift
+++ GMLXcodePlugin/Service/ClassService/PropertyMethodBuilder.swift
@@ -88,7 +88,7 @@ fileprivate extension PropertyMethodBuilder {
         var selectTexts = [String]()
         for textRange in selections {
             var selectText = String()
-            for line in textRange.start.line..<textRange.end.line {
+            for line in textRange.start.line ... textRange.end.line {
                 var start: Int? = nil
                 var end: Int? = nil
                 if line == textRange.start.line {
@@ -185,68 +185,6 @@ fileprivate extension PropertyMethodBuilder {
         }
         return (methods, methodText)
     }
-    
-    /**
-     static inline id _MASBoxValue(const char *type, ...) {
-         va_list v;
-         va_start(v, type);
-         id obj = nil;
-         if (strcmp(type, @encode(id)) == 0) {
-             id actual = va_arg(v, id);
-             obj = actual;
-         } else if (strcmp(type, @encode(CGPoint)) == 0) {
-             CGPoint actual = (CGPoint)va_arg(v, CGPoint);
-             obj = [NSValue value:&actual withObjCType:type];
-         } else if (strcmp(type, @encode(CGSize)) == 0) {
-             CGSize actual = (CGSize)va_arg(v, CGSize);
-             obj = [NSValue value:&actual withObjCType:type];
-         } else if (strcmp(type, @encode(MASEdgeInsets)) == 0) {
-             MASEdgeInsets actual = (MASEdgeInsets)va_arg(v, MASEdgeInsets);
-             obj = [NSValue value:&actual withObjCType:type];
-         } else if (strcmp(type, @encode(double)) == 0) {
-             double actual = (double)va_arg(v, double);
-             obj = [NSNumber numberWithDouble:actual];
-         } else if (strcmp(type, @encode(float)) == 0) {
-             float actual = (float)va_arg(v, double);
-             obj = [NSNumber numberWithFloat:actual];
-         } else if (strcmp(type, @encode(int)) == 0) {
-             int actual = (int)va_arg(v, int);
-             obj = [NSNumber numberWithInt:actual];
-         } else if (strcmp(type, @encode(long)) == 0) {
-             long actual = (long)va_arg(v, long);
-             obj = [NSNumber numberWithLong:actual];
-         } else if (strcmp(type, @encode(long long)) == 0) {
-             long long actual = (long long)va_arg(v, long long);
-             obj = [NSNumber numberWithLongLong:actual];
-         } else if (strcmp(type, @encode(short)) == 0) {
-             short actual = (short)va_arg(v, int);
-             obj = [NSNumber numberWithShort:actual];
-         } else if (strcmp(type, @encode(char)) == 0) {
-             char actual = (char)va_arg(v, int);
-             obj = [NSNumber numberWithChar:actual];
-         } else if (strcmp(type, @encode(bool)) == 0) {
-             bool actual = (bool)va_arg(v, int);
-             obj = [NSNumber numberWithBool:actual];
-         } else if (strcmp(type, @encode(unsigned char)) == 0) {
-             unsigned char actual = (unsigned char)va_arg(v, unsigned int);
-             obj = [NSNumber numberWithUnsignedChar:actual];
-         } else if (strcmp(type, @encode(unsigned int)) == 0) {
-             unsigned int actual = (unsigned int)va_arg(v, unsigned int);
-             obj = [NSNumber numberWithUnsignedInt:actual];
-         } else if (strcmp(type, @encode(unsigned long)) == 0) {
-             unsigned long actual = (unsigned long)va_arg(v, unsigned long);
-             obj = [NSNumber numberWithUnsignedLong:actual];
-         } else if (strcmp(type, @encode(unsigned long long)) == 0) {
-             unsigned long long actual = (unsigned long long)va_arg(v, unsigned long long);
-             obj = [NSNumber numberWithUnsignedLongLong:actual];
-         } else if (strcmp(type, @encode(unsigned short)) == 0) {
-             unsigned short actual = (unsigned short)va_arg(v, unsigned int);
-             obj = [NSNumber numberWithUnsignedShort:actual];
-         }
-         va_end(v);
-         return obj;
-     }
-     */
 }
 //MARK:- Property Info
 fileprivate extension PropertyMethodBuilder {
